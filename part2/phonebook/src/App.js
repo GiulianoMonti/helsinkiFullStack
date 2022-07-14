e3e69469bd07d5118b6f0c980bbb105a10c5e416
giuliano monti <giulianm@gmail.com>
2022-07-14 13:32:22 -0300

    2.15 2.16

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,43 +1,39 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import personService from "./services/personService";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/persons").then((response) => {
-      console.log("promise fulfilled");
-      setPersons(response.data);
+    personService.getAll().then((response) => {
+      setPersons(response);
     });
   }, []);
-
-  const personsToShow = persons
-    .filter((p) => p.name.toLowerCase().includes(filter.toLowerCase()))
-    .map((result) => result);
 
   const handleNameChange = (e) => {
     e.preventDefault();
     setNewName(e.target.value);
   };
+
   const handleNumberChange = (e) => {
     e.preventDefault();
     setNewNumber(e.target.value);
   };
+
   const handleSearch = (e) => {
     e.preventDefault();
-    setFilter(e.target.value);
+    setSearch(e.target.value);
   };
 
   const addPerson = (e) => {
     e.preventDefault();
-    //Prevent the user from being able to add names that already exist in the phonebook.
+
     if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       return;
@@ -47,13 +43,24 @@ const App = () => {
       number: newNumber,
     };
 
-    setPersons(persons.concat(personObject));
+    personService.create(personObject).then((response) => {
+      setPersons(persons.concat(response));
+      setNewName("");
+      setNewNumber("");
+    });
   };
+  const personsToShow = persons
+
+    .filter((person) =>
+      person.name.toLowerCase().includes(search.toLowerCase())
+    )
+    .map((result) => result);
+  // setPersons(persons.concat(personObject));
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter handleSearch={handleSearch} filter={filter} />
+      <Filter handleSearch={handleSearch} search={search} />
       <PersonForm
         addPerson={addPerson}
         handleNameChange={handleNameChange}
--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Filter({ handleSearch, filter }) {
+export default function Filter({ handleSearch, search }) {
   return (
     <div>
-      filter shown with : <input value={filter} onChange={handleSearch} />
+      filter shown with : <input value={search} onChange={handleSearch} />
     </div>
   );
 }
